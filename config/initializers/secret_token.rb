58392340c050e3410e658b19385089595e7d63dc
jessicaeldredge <jessica.eldredge@livingsocial.com>
2013-02-08 09:42:57 -0500

    Activate the party app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schwank::Application.config.secret_token = '5769817303ed652869a90aa3915473f04e0cb8634e266fadf849c1207c62baee96a48043384eb48c1403455429b589a4fc391fb4213cea0f65db5dcdadd220ed'
